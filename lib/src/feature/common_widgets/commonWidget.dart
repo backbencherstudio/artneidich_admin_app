b5a651a995247c63bffd9b7891165ffd1f6659bb
df <fd>
2025-07-11 11:16:22 +0600

    done

--- lib/src/feature/common_widgets/commonWidget.dart
+++ lib/src/feature/common_widgets/commonWidget.dart
@@ -2,6 +2,7 @@ import 'package:artneidich_admin/src/feature/common_widgets/appBar/appBar.dart';
 import 'package:artneidich_admin/src/feature/common_widgets/delete_bottomsheet/delete_bottomSheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CommonWidget {
   static Widget appBar({String? title}) =>
@@ -9,6 +10,6 @@ class CommonWidget {
 
 
  static Future<void> deleteSheet({required BuildContext context , required Function onTap}) async {
-   await showDeleteSheet(context : context, onTap: onTap);
+   await showDeleteSheet(context : context, onTap: onTap,);
  }
 }
